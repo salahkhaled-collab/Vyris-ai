85da82d7aa7c5ba13952a6d0557f9dfa97200703
Salah khaled <salaheldinkhaled12@gmail.com>
2026-09-19 18:50:17 +0000

    disable dangerous email linking

--- src/lib/auth-options.ts
+++ src/lib/auth-options.ts
@@ -21,7 +21,7 @@ export const authOptions: NextAuthOptions = {
     GoogleProvider({
       clientId: process.env.GOOGLE_CLIENT_ID as string,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
-      allowDangerousEmailAccountLinking: true,
+      allowDangerousEmailAccountLinking: false,
       authorization: {
         params: {
           scope: [
