bcfc68d8749ebd084decc61e85f387b3e4cec9c8
Salah khaled <salaheldinkhaled12@gmail.com>
2026-09-23 19:20:52 +0000

    src/app/api/gmail/[id]/route.ts

--- src/app/api/gmail/[id]/route.ts
+++ src/app/api/gmail/[id]/route.ts
@@ -2,6 +2,7 @@ import { NextResponse } from "next/server";
 import { getServerSession } from "next-auth";
 import { authOptions } from "@/lib/auth-options";
 import { getGoogleAccessToken } from "@/lib/google-token";
+export const dynamic = "force-dynamic";
 
 interface GmailHeader {
   name: string;
