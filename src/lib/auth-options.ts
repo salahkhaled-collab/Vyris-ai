14228884e166adfccefc2b8f057a930ebbd42681
Salah khaled <salaheldinkhaled12@gmail.com>
2026-09-23 12:30:11 +0000

    src/lib/auth-options.ts

--- src/lib/auth-options.ts
+++ src/lib/auth-options.ts
@@ -24,11 +24,12 @@ export const authOptions: NextAuthOptions = {
       allowDangerousEmailAccountLinking: false,
       authorization: {
         params: {
-        scope: [
+         scope: [
   "openid",
   "email",
   "profile",
   "https://www.googleapis.com/auth/calendar",
+  "https://www.googleapis.com/auth/gmail.readonly",
 ].join(" "),
           access_type: "offline",
           prompt: "consent",
